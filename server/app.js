511a44ea16b3516b965dcdc48b83de64a3a707d7
Amit Kumar Patel <patelamyt@gmail.com>
2023-06-27 18:16:19 +0545

    CORS setting

--- server/app.js
+++ server/app.js
@@ -3,6 +3,16 @@ const { User } = require('./model');
 
 const app = express();
 app.use(express.json());
+app.use(cors({origin:'*',credentials:true}));
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+});
 
 app.get('/users', async (req, res, next) => {
     const agents = await User.findAll();
