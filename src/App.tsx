ebea0757858cb3eb0c0ff90118fe0bb7f67bb219
Amit Kumar Patel <patelamyt@gmail.com>
2023-06-27 16:25:14 +0545

    backend test

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const[data,setData]=useState([]);
+  async function get(){
+    const response = await fetch("http://example.com/movies.json");
+    const jsonData = await response.json();
+    setData(jsonData);
+  }
+
+  useEffect(() => {
+    get();
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {data}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
